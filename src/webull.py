93fd696e10c752590f68f2c45b95e4b050c4cf4d
Miguel Mendoza <miguelmcell@gmail.com>
2021-01-12 23:23:25 -0600

    added syncWebull endpoint

--- src/webull.py
+++ src/webull.py
@@ -70,7 +70,7 @@ class Webull(commands.Cog):
                 webull_account = broker
         if not found_webull:
             # tell them to add webull account first
-            await ctx.send('Add a webull account first before changing email 🙂')
+            await ctx.send('Run `!addWebullBroker` before changing email 🙂')
         else:
             # change existing webull email
             pass
@@ -106,4 +106,39 @@ class Webull(commands.Cog):
         if await handle_api_response(ctx, r) is None:
             return
 
-        await ctx.send('Webull MFA code has been sent to {}, use MFA code to connect to your webull account using `!connect`\nuse `help !connect` for more info'.format(webull_account['brokerUsername']))
+        await ctx.send('Webull MFA code has been sent to {}, use MFA code to connect to your webull account using `!syncWebull`\nuse `help !connect` for more info'.format(webull_account['brokerUsername']))
+
+    @commands.command(name='syncWebull', \
+         help='Connects to the user\'s webull account to sync performance and holdings',
+         aliases=['syncwebull'])
+    @commands.dm_only()
+    async def syncWebull(self, ctx, password, mfaCode):
+        # validate user has a finbhub account
+        # print("PASSWORD IS:", password.strip('"'))
+        user = await self.Broker.getUser(ctx)
+        if user is None:
+            await ctx.send('Unable to find a finhub discord server associated with user')
+            return
+        elif user['discordId'] != str(ctx.message.author.id):
+            await ctx.send('Request discordId does not match own userId')
+            return
+
+        user_broker_accounts = user['brokers']
+        found_webull = False
+        webull_account = {}
+        for broker in user_broker_accounts:
+            if broker['name'] == 'webull':
+                found_webull = True
+                webull_account = broker
+        if not found_webull:
+            # tell them to add webull account first
+            await ctx.send('Run `!addWebullBroker` before connecting to account 🙂')
+            return
+        # call sync endpoint
+        url = self.ENDPOINTS[self.ENVIRONMENT]['host']+self.ENDPOINTS[self.ENVIRONMENT]['webull']['sync_webull']
+        data = {'discordId': ctx.message.author.id, 'email': webull_account['brokerUsername'], 'password': password, 'mfaCode': mfaCode}
+        r = await call_post_request(ctx, url, data=data)
+        if await handle_api_response(ctx, r) is None:
+            return
+
+        await ctx.send('Webull is now synced into your account!, run `!status` for an overview of your account status')
